3074653661423759c260af255c5af452a4e3512c
AryanGup01 <20ucs034@lnmiit.ac.in>
2024-05-19 23:06:37 +0530

    git ignore updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      '/api': 'http://localhost:8000'
+      '/api': 'https://videotube-backend-bynh.onrender.com'
     }
   },
   plugins: [react()],
