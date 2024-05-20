f76243c0426eaa5140bd1b63f6edb17957576037
Sankalp Gupta <guptasankalp2004@gmail.com>
2024-05-20 05:43:04 +0530

    alert remove

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -51,7 +51,6 @@ const Register = () => {
             response.then(navigate('/login'))
             } catch (error) {
                 setLoading(false)
-                alert("error occurred try again");
         }
     } else{
         alert("pasword do not match")
