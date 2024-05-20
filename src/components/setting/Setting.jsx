f6f3db7ff8549e83db62d8862bbd3ad7b88a1955
Sankalp Gupta <guptasankalp2004@gmail.com>
2024-05-20 05:48:16 +0530

    mobile responsive settings page

--- src/components/setting/Setting.jsx
+++ src/components/setting/Setting.jsx
@@ -17,36 +17,36 @@ const Setting = () => {
 
     return (
         <div className='w-full h-[100vh] flex py-10 justify-center items-center' >
-            <div className='w-2/3 h-2/4  bg-black/90 rounded-2xl '>
-            <div className="theme flex justify-between mx-40 text-white my-10  text-xl items-center">
-                <p>Theme</p>
-                <select 
-                name="theme" 
-                value={select}
-                onChange={(e) => {
-                    setSelect(e.target.value)
-                }}
-                className='  outline-none border-none px-5 py-1 rounded-xl text-black text-xl'
-                >
-                    <option value="light" > Light</option>
-                    <option value="dark" defaultChecked> Dark</option>
-                </select>
-            </div>
-            <div className="deleteChannel text-white">
-                <button className='bg-red-700 px-10 py-3 rounded-full mx-40 my-5 hover:bg-red-800'>
-                    Clear watch History
-                </button>
-            </div>
-            <div className="logout text-white">
-                <button className='bg-red-700 px-10 py-3 rounded-full mx-40 hover:bg-red-800' onClick={logoutHandler}>
-                    Log Out
-                </button>
-            </div>
-            <div className="deleteChannel text-white">
-                <button className='bg-red-700 px-10 py-3 rounded-full mx-40 my-5 hover:bg-red-800'>
-                    Delete Channel
-                </button>
-            </div>
+            <div className='w-2/3 h-2/4  bg-black/90 max-md:bg-transparent rounded-2xl flex justify-center items-center'>
+                <div className="theme flex justify-between mx-40 text-white my-10  text-xl items-center">
+                    <p>Theme</p>
+                    <select 
+                    name="theme" 
+                    value={select}
+                    onChange={(e) => {
+                        setSelect(e.target.value)
+                    }}
+                    className='  outline-none border-none px-5 py-1 rounded-xl text-black text-xl'
+                    >
+                        <option value="light" > Light</option>
+                        <option value="dark" defaultChecked> Dark</option>
+                    </select>
+                </div>
+                <div className="deleteChannel text-white">
+                    <button className='bg-red-700 px-10 py-3 rounded-full mx-40 my-5 hover:bg-red-800'>
+                        Clear watch History
+                    </button>
+                </div>
+                <div className="logout text-white">
+                    <button className='bg-red-700 px-10 py-3 rounded-full mx-40 hover:bg-red-800' onClick={logoutHandler}>
+                        Log Out
+                    </button>
+                </div>
+                <div className="deleteChannel text-white">
+                    <button className='bg-red-700 px-10 py-3 rounded-full mx-40 my-5 hover:bg-red-800'>
+                        Delete Channel
+                    </button>
+                </div>
             </div>
         </div>
     )
