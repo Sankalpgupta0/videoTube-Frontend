2b285fdaae9e03292eb4c1addf6f75b775ccc8a8
Sankalp Gupta <guptasankalp2004@gmail.com>
2024-05-20 12:11:50 +0530

    minor frontend changes

--- src/components/community/Message.jsx
+++ src/components/community/Message.jsx
@@ -16,10 +16,10 @@ const Message = ({owner, content}) => {
 
     return (
         <div className= {`w-full flex ${currentUser._id != owner ? "flex-row-reverse" : "flex-row"}` }>
-            <div className='w-1/2'>
+            <div className='w-1/2 max-md:hidden'>
 
             </div>
-            <div className={`text-white w-1/2  h-fit flex flex-col px-10 py-5 my-1 rounded-2xl ${currentUser._id != owner ? "bg-gray-700" : "bg-green-700"}`}>
+            <div className={`text-white w-1/2 max-md:w-full h-fit flex flex-col px-10 py-5 max-md:px-1 my-1 rounded-2xl ${currentUser._id != owner ? "bg-gray-700" : "bg-green-700"}`}>
                 <h1 className='ml-5'>
                     {content}
                 </h1>
