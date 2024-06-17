e7e70886c90481014f78d0f8d179bb1efb24ea5d
Sankalp Gupta <guptasankalp2004@gmail.com>
2024-06-17 14:20:52 +0530

    websocket implement

--- src/components/community/Chat.jsx
+++ src/components/community/Chat.jsx
@@ -1,23 +1,70 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Message from './Message'
 import axios from 'axios'
-import Input from './Input'
+
 
 const Chat = () => {
-    const [messages, setMessages] = useState([])
+    const [msgFromDB, setMsgFromDB] = useState([])
+    const [msgFromSockets, setMsgFromSockets] = useState([])
+    const [input, setInput] = useState("")
+    const [currentUser, setCurrentUser] = useState('')
+    const socketRef = useRef(null);
 
     const getMessages = () => {
         axios.get('/api/tweets')
-            .then(res => setMessages(res.data.data.tweets))
+            .then(res => setMsgFromDB(res.data.data.tweets))
     }
+
+    const getcurrentUser = async () => {
+        const url = '/api/users/current-user'
+        const response = await axios.get(url)
+        // console.log(response.data.data);
+        setCurrentUser(response.data.data)
+        connectSocket({userId:response.data.data._id})
+    }
+
+    const sendMsg = () => {
+        if (socketRef.current.readyState === WebSocket.OPEN) {
+            socketRef.current.send(input);
+            // console.log(`Sent message => ${input}`);
+            setInput('');
+        } else {
+            console.error('WebSocket is not open');
+        }
+    };
+
+    const connectSocket = ({userId}) => {
+        // Create a WebSocket connection
+        socketRef.current = new WebSocket(`ws://localhost:3000?_id=${userId}`);
+        console.log("connected");
+
+        // Handle incoming messages
+        socketRef.current.onmessage = (event) => {
+            // console.log(event);
+            setMsgFromSockets((prevMsgs) => [...prevMsgs, event.data]);
+            // console.log(`Received message => ${event.data}`);
+        };
+
+        // Handle WebSocket connection close
+        socketRef.current.onclose = () => {
+            console.log('WebSocket connection closed');
+        };
+
+        // Handle WebSocket connection errors
+        socketRef.current.onerror = (error) => {
+            console.error('WebSocket error:', error);
+        };
+    };
+
     useEffect(() => {
+        getcurrentUser()
         getMessages()
     }, [])
     return (
-        <div className='w-full h-[95vh] px-10 ' >
-            <div className='h-4/5 w-full overflow-y-scroll'>
+        <div className='w-full inputHeight px-10 ' >
+            <div className='h-[90%] w-full overflow-y-scroll'>
                 {
-                    messages.map((message, index) => (
+                    msgFromDB.map((message, index) => (
                         <Message
                             key={index}
                             owner={message.owner}
@@ -25,8 +72,42 @@ const Chat = () => {
                         />
                     ))
                 }
+                {
+                    msgFromSockets.map((message, index) => {
+                        message = JSON.parse(message)
+                        return(
+                            <Message
+                                key={index}
+                                owner={message.owner}
+                                content={message.content}
+                            />
+                        )
+                        })
+                }
             </div>
-            <Input />
+
+            <div
+                onKeyDown={(e) => {
+                    if (e.key == "Enter")
+                        sendMsg()
+                }}
+                className='w-full h-[10%]  absolute bottom-0 px-10 '
+            >
+                <div className='w-full h-full flex rounded-2xl overflow-hidden'>
+                    <input
+                        type="text"
+                        className='w-[90%] h-full bg-white px-5 py-5 text-xl border-none outline-none '
+                        placeholder='Enter your thoughts...'
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button
+                        onClick={sendMsg}
+                        className='w-[10%] rounded-tr-2xl rounded-br-2xl h-full bg-green-500 text-center hover:bg-green-700 text-white text-xl font-extrabold'>
+                        Post
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/community/Message.jsx
+++ src/components/community/Message.jsx
@@ -17,11 +17,11 @@ const Message = ({ owner, content }) => {
 
     return (
         <div className={`w-full flex ${currentUser._id != owner ? "flex-row-reverse" : "flex-row"}`}>
-            <div className='w-1/2 max-md:hidden'>
+            <div className='w-2/5 max-md:hidden'>
 
             </div>
-            <div className={`text-white w-1/2 max-md:w-full overflow-x-auto  h-fit flex flex-col px-5 py-2 max-md:px-1 my-1 rounded-2xl  ${currentUser._id != owner ? "bg-gray-700" : "bg-green-700"}`}>
-                <h1 className='ml-5 text-pretty '>
+            <div className={`text-white w-3/5 max-md:w-full overflow-x-hidden  h-fit flex flex-col px-5 py-2 max-md:px-1 my-1 rounded-2xl  ${currentUser._id != owner ? "bg-gray-700" : "bg-green-700"}`}>
+                <h1 className='ml-5 textOverflowHor'>
                     {content}
                 </h1>
                 <p className='text-xs text-gray-200 cursor-pointer my-2' onClick={() => navigate(`/home/channel/${owner}`)}>by: {owner}</p>
